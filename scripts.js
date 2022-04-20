fa0d73335408168c8ae8dc276a439d79e23ef6b4
lucascr <lcrodrigues1995@gmail.com>
2022-04-20 18:35:06 -0300

    Update content

--- scripts.js
+++ scripts.js
@@ -17,44 +17,44 @@ const speakersDetails = [
   {
     id: 1,
     picture: 'Art/speaker_01.png',
-    name: 'Yochai Benkler',
-    occupation: 'Berkman Professor of Entrepreneurial Legal Studies at Harvard Law School',
-    description: 'Benkler studies commons-based peer production, and published his seminal book "The Wealth of Networks" in 2006'
+    name: 'Dra. Jane Goodall',
+    occupation: 'World-renowned ethologist and conservationist',
+    description: 'World-renowned ethologist and conservationist, famous for her study of wild chimpanzees in Gombe, Tanzania.'
   },
   {
     id: 2,
-    picture: 'Art/speaker_01.png',
-    name: 'Yochai Benkler2',
-    occupation: 'Berkman Professor of Entrepreneurial Legal Studies at Harvard Law School',
-    description: 'Benkler studies commons-based peer production, and published his seminal book "The Wealth of Networks" in 2006'
+    picture: 'Art/speaker_02.png',
+    name: 'Sarah Bouchie',
+    occupation: "Lego Foundation's Director of Impact",
+    description: "Sarah leads the development and implementation of the Foundation's initiatives, which aim to inspire and equip the world to embrace learning through games"
   },
   {
     id: 3,
-    picture: 'Art/speaker_01.png',
-    name: 'Yochai Benkler3',
-    occupation: 'Berkman Professor of Entrepreneurial Legal Studies at Harvard Law School',
-    description: 'Benkler studies commons-based peer production, and published his seminal book "The Wealth of Networks" in 2006'
+    picture: 'Art/speaker_03.png',
+    name: 'Kevin McGrew',
+    occupation: 'Director of the Institute of Applied Psychometrics',
+    description: 'PhD in educational psychology from the University of Minnesota and visiting professor at the same university'
   },
   {
     id: 4,
-    picture: 'Art/speaker_01.png',
-    name: 'Yochai Benkler4',
-    occupation: 'Berkman Professor of Entrepreneurial Legal Studies at Harvard Law School',
-    description: 'Benkler studies commons-based peer production, and published his seminal book "The Wealth of Networks" in 2006'
+    picture: 'Art/speaker_04.png',
+    name: 'Roberto Lent',
+    occupation: 'Post-doctorate in Biophysics at the Massachusetts Institute of Technology',
+    description: 'He is a member of the Brazilian Academy of Sciences and Coordinator of the National Network of Science for Education'
   },
   {
     id: 5,
-    picture: 'Art/speaker_01.png',
-    name: 'Yochai Benkler5',
-    occupation: 'Berkman Professor of Entrepreneurial Legal Studies at Harvard Law School',
-    description: 'Benkler studies commons-based peer production, and published his seminal book "The Wealth of Networks" in 2006'
+    picture: 'Art/speaker_05.png',
+    name: 'Ricardo Primi',
+    occupation: 'Doctor in School Psychology and Human Development from USP/Yale',
+    description: 'Associate professor at the Universidade São Francisco and part of the scientific council of the Ayrton Senna Institute'
   },
   {
     id: 6,
-    picture: 'Art/speaker_01.png',
-    name: 'Yochai Benkler6',
-    occupation: 'Berkman Professor of Entrepreneurial Legal Studies at Harvard Law School',
-    description: 'Benkler studies commons-based peer production, and published his seminal book "The Wealth of Networks" in 2006'
+    picture: 'Art/speaker_06.png',
+    name: 'William Kamkwamba',
+    occupation: 'Inventor and bestselling author',
+    description: 'William recounts his story in his memoir The Boy Who Harnessed the Wind: Creating Currents of Electricity and Hope'
   }
 ]
 
@@ -68,7 +68,7 @@ function loadSpeakers() {
         </div>
         <div class="speakers-container">
           <div class="speakers" id="1">
-            <img src="${speakersDetails[0].picture}" alt="">
+          <div class="speakerImg"><img src="${speakersDetails[0].picture}" alt=""></div>
             <div class="speakersInfo">
               <h3>${speakersDetails[0].name}</h3>
               <h4>${speakersDetails[0].occupation}</h4>
@@ -77,7 +77,7 @@ function loadSpeakers() {
             </div>
           </div>
           <div class="speakers" id="2">
-            <img src="${speakersDetails[1].picture}" alt="">
+            <div class="speakerImg"><img src="${speakersDetails[1].picture}" alt=""></div>
             <div class="speakersInfo">
               <h3>${speakersDetails[1].name}</h3>
               <h4>${speakersDetails[1].occupation}</h4>
@@ -86,7 +86,7 @@ function loadSpeakers() {
             </div>
           </div>
           <div class="speakers hideSpeakers" id="3">
-            <img src="${speakersDetails[2].picture}" alt="">
+            <div class="speakerImg"><img src="${speakersDetails[2].picture}" alt=""></div>
             <div class="speakersInfo">
               <h3>${speakersDetails[2].name}</h3>
               <h4>${speakersDetails[2].occupation}</h4>
@@ -95,7 +95,7 @@ function loadSpeakers() {
             </div>
           </div>
           <div class="speakers hideSpeakers" id="4">
-            <img src="${speakersDetails[3].picture}" alt="">
+            <div class="speakerImg"><img src="${speakersDetails[3].picture}" alt=""></div>
             <div class="speakersInfo">
               <h3>${speakersDetails[3].name}</h3>
               <h4>${speakersDetails[3].occupation}</h4>
@@ -104,7 +104,7 @@ function loadSpeakers() {
             </div>
           </div>
           <div class="speakers hideSpeakers" id="5">
-            <img src="${speakersDetails[4].picture}" alt="">
+            <div class="speakerImg"><img src="${speakersDetails[4].picture}" alt=""></div>
             <div class="speakersInfo">
               <h3>${speakersDetails[4].name}</h3>
               <h4>${speakersDetails[4].occupation}</h4>
@@ -113,7 +113,7 @@ function loadSpeakers() {
             </div>
           </div>
           <div class="speakers hideSpeakers" id="6">
-            <img src="${speakersDetails[5].picture}" alt="">
+            <div class="speakerImg"><img src="${speakersDetails[5].picture}" alt=""></div>
             <div class="speakersInfo">
               <h3>${speakersDetails[5].name}</h3>
               <h4>${speakersDetails[5].occupation}</h4>
